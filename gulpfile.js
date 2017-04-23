cb6d2777a51ca04a627a585a6fd3d8992acf18ce
Aaron <ahsu1230@gmail.com>
2017-04-23 00:05:41 -0700

    Setup gulp for new setup

---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+'use strict';
+var args   = require('yargs').argv;
+var browserSync = require('browser-sync').create();
+var concat = require('gulp-concat');
+var del = require('del');
+var gulp = require('gulp');
+var gulpif = require('gulp-if');
+var order = require('gulp-order');
+var pug = require('gulp-pug');
+var runSequence = require('run-sequence');
+var stylus = require('gulp-stylus');
+var uglify = require('gulp-uglify');
+
+var release = args.release ? true : false; // (gulp --release)
+
+var buildFolder = 'build';
+var jsFolder = 'src/js/**/*.js';
+var pugFolder = 'src/**/*.pug'
+var stylFolder = "src/**/*.styl";
+	
+gulp.task('clean', function() {
+	return del.sync(buildFolder);
+});
+
+gulp.task('stylus', function() {
+  return gulp.src(stylFolder)
+    .pipe(stylus({
+        'include css': true
+    }))
+    .pipe(concat('out.css'))
+    .pipe(gulp.dest(buildFolder))
+    .pipe(browserSync.reload({
+      stream:true
+    }));
+});
+
+gulp.task('pug', function() {
+  return gulp.src(pugFolder)
+    .pipe(pug())
+    .pipe(gulp.dest(buildFolder))
+    .pipe(browserSync.reload({
+      stream:true
+    }));
+});
+
+gulp.task('js', function() {
+  return gulp.src(jsFolder)
+    .pipe(concat('main.js'))
+    .pipe(gulpif(release, uglify())) // only minify if production (gulp --release)
+    .pipe(gulp.dest(buildFolder + '/js'))
+    .pipe(browserSync.reload({
+      stream:true
+    }));
+});
+
+gulp.task('libs', function() {
+  return gulp.src('src/libs/**/*')
+    .pipe(order([
+      "jquery.js",
+      "lodash.js",
+    ]))
+    .pipe(concat('libs.js'))
+    .pipe(gulp.dest(buildFolder + '/libs'));
+});
+
+gulp.task('initBrowserSync', function() {
+	browserSync.init({
+		server: {
+			baseDir: 'build'
+		},
+	});
+});
+
+
+gulp.task('watch', function () {
+	gulp.watch(jsFolder, ['js']);
+	gulp.watch(pugFolder, ['pug']);
+	gulp.watch(stylFolder, ['stylus']);
+});
+
+gulp.task('default', function() {
+    runSequence('clean', 'libs', 'js', 'pug', 'stylus', 'initBrowserSync', 'watch');
+});
