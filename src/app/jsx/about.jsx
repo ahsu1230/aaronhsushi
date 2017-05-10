222fa3a958edc067f132728e5f24dd3f73accf33
Aaron Hsu <aaron@nextbit.com>
2017-05-09 17:58:07 -0700

    More content in about page

--- src/app/jsx/about.jsx
+++ src/app/jsx/about.jsx
@@ -35,7 +35,17 @@ class AboutBanner extends React.Component {
 class AboutContent extends React.Component {
 	render() {
 		return (
-			<div>
+			<div id="content-about">
+				<div className="about-content-section">
+					<img src="assets/sushi_boat_ice0.jpg"/>
+					<span>
+						Born and raised in Potomac, Maryland, Aaron ate his first sushi at the age of 10 and loved sushi since. The restaurant that will always have a place in his heart is Yuraku in Germantown, MD. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus eleifend mi. Cras ut tristique nisl, et facilisis ex. Suspendisse efficitur lorem diam, ut ultricies leo porttitor id. In hendrerit porttitor arcu, id fringilla magna suscipit sit amet. Sed tincidunt mi ut arcu laoreet, sed rutrum leo lobortis. Sed ac urna in ex posuere pulvinar. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed nisl tortor. Nulla facilisi.
+					</span>
+				</div>
+				<div className="about-content-section">
+					<h3>Connect with me</h3>
+
+				</div>
 			</div>
 		);
 	}
