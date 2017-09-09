73feeba1b15fc0781ff8e50bc947d049a529e146
Aaron Hsu <aaron@nextbit.com>
2017-09-08 17:29:35 -0700

    Add handle functions to footer prev and next

--- src_react/app/jsx/galleryWideLayout.jsx
+++ src_react/app/jsx/galleryWideLayout.jsx
@@ -1,20 +1,45 @@
 'use strict';
-require('./../styles/galleryWide.styl'); 
+require('./../styles/galleryWide.styl');
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ContentList } from './contentList.jsx';
 import { ContentMap } from './contentMap.jsx';
 
 export class WideLayout extends React.Component {
+	constructor() {
+			super();
+			this.state = {
+				currentIndex: 0
+			};
+			this.handlePrev = this.handlePrev.bind(this);
+			this.handleNext = this.handleNext.bind(this);
+	}
+
+	handlePrev() {
+		var listSize = ContentList.length;
+		this.setState({
+			currentIndex: (this.state.currentIndex - 1 + listSize) % listSize
+		});
+	}
+
+	handleNext() {
+		var listSize = ContentList.length;
+		this.setState({
+			currentIndex: (this.state.currentIndex + 1) % listSize
+		});
+	}
+
 	render() {
 		const show = this.props.show;
 		const showClassName = show ? "show" : "";
-		
-		var list = getContents();
+		const currentIndex = this.state.currentIndex;
+
+		const list = getContents();
 		return (
 			<div id="gallery-wide-view" className={showClassName}>
 				<div id="gallery-wide-content">
-					<WideFooter list={list}/>
+					<WideImage list={list} currentIndex={currentIndex}/>
+					<WideFooter list={list} currentIndex={currentIndex} handlePrevFunc={this.handlePrev} handleNextFunc={this.handleNext}/>
 				</div>
 			</div>
 		);
@@ -30,22 +55,39 @@ function getContents() {
 	return tiles;
 }
 
+class WideImage extends React.Component {
+	render() {
+		var list = this.props.list;
+		var currentIndex = this.props.currentIndex;
+		var content = list[currentIndex];
+		const imgSrc = content.source.fullUrl;
+		return (
+			<div className="wide-content-wrapper">
+				<img src={imgSrc} className="wide-content-img"/>
+			</div>
+		);
+	}
+}
+
 class WideFooter extends React.Component {
 	render() {
 		var list = this.props.list;
-		const dots = list.map(content =>
-			<Dot key={content.id}/>
-		)
+		const currentIndex = this.props.currentIndex;
+
+		const dots = list.map((content, index) =>
+			<Dot key={content.id} selected={index==currentIndex}/>
+		);
+
 		return (
 			<div id="gallery-wide-footer">
-				<button className="wide-prev-btn" onClick={this.handlePrev}>
-					<img src="assets/arrow_left_black.svg"/>
+				<button className="wide-prev-btn">
+					<img src="assets/arrow_left_white.svg" onClick={this.props.handlePrevFunc}/>
 				</button>
 				<div id="gallery-wide-dots">
 					{dots}
 				</div>
-				<button className="wide-next-btn" onClick={this.handleNext}>
-					<img src="assets/arrow_right_black.svg"/>
+				<button className="wide-next-btn">
+					<img src="assets/arrow_right_white.svg" onClick={this.props.handleNextFunc}/>
 				</button>
 			</div>
 		);
