53266b5d173ca450e44aacec617342c24e229d97
Aaron Hsu <aaron@nextbit.com>
2021-08-05 15:09:58 -0400

    footer and fonts and colors

--- site/src/footer/footer.js
+++ site/src/footer/footer.js
@@ -1,3 +1,19 @@
+import "./footer.sass";
+import logo from "./../assets/logo_hsu2a.png";
+import iconIG from "./../assets/instagram_white.svg";
+import iconLI from "./../assets/linkedin_white.svg";
 export default function Footer() {
-    return <div>Footer</div>;
+    return (
+        <div id="footer">
+            <img className="logo" src={logo} />
+            <p>Designed and Developed by Aaron Hsu</p>
+
+            <h4>Contact</h4>
+            <p>ahsu1230@gmail.com</p>
+            <div className="icons">
+                <img src={iconIG} />
+                <img src={iconLI} />
+            </div>
+        </div>
+    );
 }
