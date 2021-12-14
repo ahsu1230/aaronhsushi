b894bce073f874dba692febfb86ac669553e4e50
Aaron Hsu <aaron@nextbit.com>
2021-12-14 16:14:55 -0500

    Switch some how to videos

--- src/inspiration/inspirationHowTo.js
+++ src/inspiration/inspirationHowTo.js
@@ -13,20 +13,24 @@ export default function InspirationHowTo(props) {
             </p>
             <div className="youtube-cards">
                 <YoutubeCard
+                    title={"Just One Cookbook "}
+                    description={"Nami-san is a blogger with an extraordinarily extensive knowledge about Japanese cooking. Check out her website to learn more about authentic Japanese recipes!"}
+                    url={"https://www.youtube.com/embed/-gsEDlfYxeU"}
+                />
+                <YoutubeCard
                     title={"Akira-san's Home Sushi Channel"}
+                    description={"A fun channel dedicated to sushi made by a Japanese couple who have worked professionally in an established Tokyo sushi restaurant."}
                     url={"https://www.youtube.com/embed/Myt-BZFyMm8"}
                 />
                 <YoutubeCard
                     title={"Ginza Watari"}
+                    description={"A sushi chef from Tokyo shares his many ideas and techniques in his video series. All of his videos are in Japanese, but some have English translations!"}
                     url={"https://www.youtube.com/embed/Iqdw93m_gpM"}
                 />
                 <YoutubeCard
-                    title={"Making sushi with Iron Chef Morimoto"}
-                    url={"https://www.youtube.com/embed/EGeNKGosXA8"}
-                />
-                <YoutubeCard
-                    title={"Hiroyuki Terada - Diaries of a Master Sushi Chef"}
-                    url={"https://www.youtube.com/embed/IqTmQ93dmLs"}
+                    title={"Samurai Sushi Spirits"}
+                    description={"A Michelin sushi chef demonstrates his techniques with very informed tutorials in his own sushi restaurant in Fukuoka, Japan."}
+                    url={"https://www.youtube.com/embed/eAagGdAVQHw"}
                 />
             </div>
         </div>
